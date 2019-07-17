9e6a2cb4cf27fe128f78f30bb4ea95a4d70ce37b
grady <1714620175@qq.com>
2019-07-17 21:25:39 +0800

    update

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -51,7 +51,7 @@ public class Test1 {
 		for (Student student : st) {
 			System.out.println(student);
 		}
-		System.out.println("--sssss---");
+		System.out.println("-----");
 		is.close();
 		
 	}
